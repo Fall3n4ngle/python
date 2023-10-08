11188e8005ee223c058509c58f7b371d77e279e4
Alex <sashaoliyar7@gmail.com>
2023-10-08 13:58:35 +0300

    completed task 1.2

--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -16,9 +16,14 @@ while True:
     data = client_socket.recv(1024).decode()
     if not data:
         break
+    
+    time.sleep(5)
     print(f"Отримано: {data}")
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(f"Час отримання: {current_time}")
+    
+    success_message = "Дані успішно отримано і оброблено."
+    client_socket.send(success_message.encode())
 
 client_socket.close()
 server_socket.close()
