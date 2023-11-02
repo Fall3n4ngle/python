c094ab4d79a07d9abc19334de3801605f2876285
Alex <sashaoliyar7@gmail.com>
2023-11-02 11:53:19 +0200

    completed task1

---
+++ task1/app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=10)])
+    remember = BooleanField('Remember Me')
+
+class TaskForm(FlaskForm):
+    title = StringField('Task Title')
+    submit = SubmitField('Add Task')
+
+class FeedbackForm(FlaskForm):
+    name = StringField('Your name')
+    message = StringField('Your message')
+    submit = SubmitField('Add feedback')
