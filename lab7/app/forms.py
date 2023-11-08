5d0ff9633da7660092cfb0af2f3cd5d3f96fff60
Alex <sashaoliyar7@gmail.com>
2023-11-09 00:10:29 +0200

    completed lab7

---
+++ lab7/app/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from app.models import User
+
+class TaskForm(FlaskForm):
+    title = StringField('Task Title')
+    submit = SubmitField('Add Task')
+
+class FeedbackForm(FlaskForm):
+    name = StringField('Your name')
+    message = StringField('Your message')
+    submit = SubmitField('Add feedback')
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=14), 
+                Regexp('^[A-Za-a][A-Za-z0-9_.]*$', 0, 'Username must only have letters, numbers, dots or underscores')       
+                                        ])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+    submit = SubmitField("Sign up")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+    
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already registered')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField("Login")
